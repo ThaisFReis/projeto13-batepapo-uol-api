600ed043c0a4fe8a4cea0b83db472e18228d6b70
ThaisFReis <reis.thaisf@gmail.com>
2022-11-14 12:53:52 -0300

    Andamento: Importei tudo o que preciso.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
 dotenv.config();
 
